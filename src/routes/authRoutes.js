400e151cdc39a58951f2c60d7a7f58d3aa449636
javieraguayo <javier.aguayo19@gmail.com>
2024-09-23 00:00:46 +0100

    Definir rutas para autenticacion, pagos y transacciones

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -3,7 +3,7 @@ const { register, login } = require('../controllers/authController');
 
 const router = express.Router();
 
-router.post('/register', register);
 router.post('/login', login);
+router.post('/register', register);
 
 module.exports = router;
