d5a2e88a5adf16d9eaf9f291c6e0fa760afe30d0
javieraguayo <javier.aguayo19@gmail.com>
2024-09-23 05:15:57 +0100

    refactor index.js cors

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ const bodyParser = require('body-parser');
 const authRoutes = require('./routes/authRoutes');
 const paymentRoutes = require('./routes/paymentRoutes');
 const transactionRoutes = require('./routes/transactionRoutes');
+const cors = require('cors');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
+app.use(cors());
 app.use(bodyParser.json());
 
 app.use('/auth', authRoutes);
